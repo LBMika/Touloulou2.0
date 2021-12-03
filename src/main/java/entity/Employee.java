d6a1ae4b9153b6938c6f5d3f6b73023e83a98cb0
LMika <lemika.pro@gmail.com>
2021-12-03 16:26:19 +0100

    Employee entity

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,82 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private int age;
+	private String roleTitle;
+	private String phoneNumber;
+	
+	
+	
+	// Getters/Setters block
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getRoleTitle() {
+		return roleTitle;
+	}
+	
+	public void setRoleTitle(String roleTitle) {
+		this.roleTitle = roleTitle;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+}
