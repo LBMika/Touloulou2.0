b6d27b960a7b76cadd957459e3f65bde0d1486ef
LMika <lemika.pro@gmail.com>
2021-12-03 16:29:07 +0100

    Location entity

---
+++ src/main/java/entity/Location.java
@@ -0,0 +1,55 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="loation")
+public class Location {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String town;
+	private String zipcode;
+	private String address;
+	
+
+
+	// Getters/Setters block
+	
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getTown() {
+		return town;
+	}
+	
+	public void setTown(String town) {
+		this.town = town;
+	}
+	
+	public String getZipcode() {
+		return zipcode;
+	}
+	
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
